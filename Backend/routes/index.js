ba07b476f511464e0b881e6eae98948b794dbbf3
cryade <PatrickGartenbach@gmx.de>
2019-12-15 15:59:30 +0100

    logincheck

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -6,5 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('This is the MyHero Backend, you are not supposed to be here');
 });
 
-
+router.get('/auth',function(req,res){
+  if (req.session.user != null ){
+    res.status(200).send(true);
+  }else{
+    res.status(400).send(false);
+  }
+});
 module.exports = router;
